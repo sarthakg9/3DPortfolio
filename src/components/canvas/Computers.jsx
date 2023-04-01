418044d8537f63e92f9c3ca45588b2ba3420b214
sarthakg9 <123239304+sarthakg9@users.noreply.github.com>
2023-04-01 15:23:11 +0530

    hero text done

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import {cSuspense, useEffect, useState } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+import CanvasLoader from '../Loader'
 
 const Computers = () => {
+  const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
-    <div>Computers</div>
+    <mesh></mesh>
   )
 }
 
