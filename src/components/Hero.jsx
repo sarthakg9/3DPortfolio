0e8cc01ffc81d4e56b5fc897d653a35df45addc3
sarthakg9 <123239304+sarthakg9@users.noreply.github.com>
2023-04-04 01:40:44 +0530

    changes in 3d computer, optimized for mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,15 +19,16 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+
+      <div className='absolute xs:bottom-10 bottom-16 w-full flex justify-center items-center'>
         <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+          <div className='w-[36px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div 
               animate = {{
-                y:[0,24,0]
+                y:[0,32,0]
               }}
               transition = {{
-                duration: 1.5,
+                duration: 2.3,
                 repeat: Infinity
                 // repeatType: 'loop'
               }}
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,7 +3,7 @@ import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import CanvasLoader from '../Loader'
 
-const Computers = () => {
+const Computers = ({ isMoblie }) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
     <mesh>
@@ -19,8 +19,8 @@ const Computers = () => {
       />
       <primitive
         object={computer.scene}
-        scale = {0.75}
-        position={[0, -4, -1.5]}
+        scale = {isMoblie ? 0.65 : 0.72}
+        position={isMoblie ? [0, -3.4, -2.2] : [0, -4.3, -1.5]}
         rotation = {[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -28,11 +28,27 @@ const Computers = () => {
 }
 
 const ComputersCanvas = () => {
+  const [isMoblie, setIsMobile] = useState(false)
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)')
+
+    setIsMobile(mediaQuery.matches)
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches)
+    }
+    mediaQuery.addEventListener('change', handleMediaQueryChange)
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    }
+  
+  }, [])
+  
   return (
     <Canvas
     frameloop='demand'
     shadows
-    camera={{position: [20,3,5], fov: 25}}
+    camera={{position: [20,3,5], fov: 30}}
     gl ={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
@@ -41,7 +57,7 @@ const ComputersCanvas = () => {
         maxPolarAngle = {Math.PI / 2}
         minPolarAngle = {Math.PI / 2}
         />
-        <Computers />
+        <Computers isMoblie={isMoblie} />
       </Suspense>
       <Preload all />
     </Canvas>
